1bc7dcf6d32b79a97a33fbe03f19f6e7817b178a
Jayesh Khandelwal <jayeshkwal2209@email.com>
2025-06-20 03:01:51 +0530

    Added leaderboard version of guessing game

---
+++ guess_the_number_leaderboard.py
@@ -0,0 +1,41 @@
+import random
+
+high_score = None
+
+print("Welcome to 'Guess the Number'!")
+print("Try to guess the number in the least attempts!")
+
+while True:
+    number_to_guess = random.randint(1, 100)
+    attempts = 0
+    guess = None
+
+    while guess != number_to_guess:
+        try:
+            guess = int(input("Enter your guess (1-100): "))
+            attempts += 1
+
+            if guess < number_to_guess:
+                print("Too low. Try again.")
+            elif guess > number_to_guess:
+                print("Too high. Try again.")
+            else:
+                print(f" Correct! You guessed it in {attempts} attempts.")
+
+                if high_score is None or attempts < high_score:
+                    high_score = attempts
+                    print(f" New High Score: {high_score} attempts!")
+                else:
+                    print(f"Current High Score: {high_score} attempts.")
+        except ValueError:
+            print(" Please enter a valid number!")
+
+    play_again = input("Play again? (yes/no): ").lower()
+    if play_again != "yes":
+        print("Thanks for playing!")
+        break
+
+
+
+
+
